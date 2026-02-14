3ca41aac7946b0611332106a459133d275254e59
reneet4nzy-jpg <reneet4n.zy@gmail.com>
2026-02-14 15:33:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,29 +7,31 @@ const messages = [
    {
   preview: "you miss my AWESOME singing",
   text: `
-    </p>hey there miguel whats it liek in tekong camp im 22km away but man tonight you look so pretty yes you do, orchard cant shine half as bright as u my word is true. 
-    \nhey there migugu dont you worry about the distance im right here if you get lonely give this sog another listen close your eyes. listen to my voice its my disguise, im by your side.</p>
+    <p>hey there miguel whats it liek in tekong camp im 22km away but man tonight you look so pretty yes you do, orchard cant shine half as bright as u my word is true.</p>
 
-    <audio controls style="width: 100%; margin-top: 10px;">
-  <source src="hey there delilah.m4a" type="audio/mp4">
-  <source src="hey there delilah.m4a" type="audio/x-m4a">
-  Your browser does not support audio.
-</audio>
+    <p>hey there migugu dont you worry about the distance im right here if you get lonely give this sog another listen close your eyes. listen to my voice its my disguise, im by your side.</p>
 
-    </p>dont judge my singing i recorded this mid coding</p>
+    <audio controls class="noteAudio">
+      <source src="hey there delilah.m4a" type="audio/mp4">
+      <source src="hey there delilah.m4a" type="audio/x-m4a">
+      Your browser does not support audio.
+    </audio>
+
+    <p>dont judge my singing i recorded this mid coding</p>
   `
 },
-  {
+{
   preview: "you miss my voice",
   text: `
-    </p>im with you always</p>
-    <audio controls style="width: 100%; margin-top: 10px;">
-  <source src="youmissmyvoice.m4a" type="audio/mp4">
-  <source src="youmissmyvoice.m4a" type="audio/x-m4a">
-  Your browser does not support audio.
-</audio>
+    <p>im with you always</p>
 
-    </p>yappidy yap yap yap</p>
+    <audio controls class="noteAudio">
+      <source src="youmissmyvoice.m4a" type="audio/mp4">
+      <source src="youmissmyvoice.m4a" type="audio/x-m4a">
+      Your browser does not support audio.
+    </audio>
+
+    <p>yappidy yap yap yap</p>
   `
 },
  {
